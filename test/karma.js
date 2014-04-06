dfa7413b4d30a79344edcad1afc521e423ccbdbe
tomchentw <developer@tomchentw.com>
2014-04-06 17:44:02 +0800

    chore(subtree): merge tomchentw/tc-ng-boilerplate@e4f805d
    Conflicts:
      .gitignore
      .travis.yml
      CHANGELOG.md
      Gemfile
      Makefile
      README.md
      bower.json
      client/gulpfile.ls
      client/javascripts/application.ls
      client/stylesheets/_bootstrap.scss
      client/stylesheets/application.scss
      client/views/index.jade
      client/views/partials/footer.jade
      client/views/partials/header.jade
      gulpfile.ls
      index.ls
      package.json
      public/CNAME
      server/gulpfile.ls
      server/index.ls
      test/karma.js
      test/protractor.js

--- test/karma.js
+++ test/karma.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/angular/angular.js',
+      'bower_components/angular/angular.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'lib/javascripts/ng-form-data.ls',
       'test/javascripts/ng-form-data.spec.ls'
